9561ba7475c99f5fc61111dc842998aedf7f643f
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-04-29 14:22:50 -0300

    Improve test printing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,22 @@
 using NLPModels, NLSProblems
 using Base.Test
 
+@printf("%-15s  %4s  %4s  %4s  %10s  %10s  %10s\n",
+        "Problem", "nequ", "nvar", "ncon", "‖F(x₀)‖²", "‖JᵀF‖",
+        "‖c(x₀)‖")
 # Test that every problem can be instantiated.
 for prob in names(NLSProblems)
   prob == :NLSProblems && continue
-  println(prob)
   prob_fn = eval(prob)
   nls = prob_fn()
 
+  N, n, m = nls.nls_meta.nequ, nls.meta.nvar, nls.meta.ncon
   x = nls.meta.x0
   Fx = residual(nls, x)
   Jx = jac_op_residual(nls, x)
-  println("  ‖F(x₀)‖ = $(norm(Fx))")
-  println("  ‖J(x₀)ᵀF(x₀)‖ = $(norm(Jx'*Fx))")
-  if nls.meta.ncon > 0
-    println("  ‖c(x₀)‖ = $(norm(cons(nls, x)))")
-  end
+  nFx = dot(Fx, Fx)
+  JtF = norm(Jx' * Fx)
+  ncx = m > 0 ? @sprintf("%10.4e", norm(cons(nls, x))) : "NA"
+  @printf("%-15s  %4d  %4d  %4d  %10.4e  %10.4e  %10s\n",
+          prob, N, n, m, nFx, JtF, ncx)
 end

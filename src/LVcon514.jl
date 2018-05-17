98204a95673a4dd5e85c63a78063879c97bbeeb5
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2018-05-17 14:03:36 -0300

    Warn instead of error on Luksan problems

--- src/LVcon514.jl
+++ src/LVcon514.jl
@@ -17,8 +17,10 @@ Chained modified HS49 problem.
 """
 function LVcon514(n :: Int = 20)
 
-  n < 5 && error(": number of variables must be ≥ 5")
-  if n % 3 != 2
+  if n < 5
+    warn(": number of variables must be ≥ 5. n = 5")
+    n = 5
+  elseif n % 3 != 2
     warn(": number of variables must be of the form 3k + 2. Rounding up")
     n = div(n - 2, 3) * 3 + 5
   end

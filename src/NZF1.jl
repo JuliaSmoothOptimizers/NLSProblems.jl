82239a021982e2bbdcf6e7fc62e5a15381e175b4
Xx-Alexis-xX <alexis.montoison@etu.enseeiht.fr>
2018-05-18 17:22:41 -0400

    NZF1 problem implemented with MathProgNLSModel

--- src/NZF1.jl
+++ src/NZF1.jl
@@ -14,21 +14,20 @@ function NZF1(n :: Int=13)
   n = 13*nbis
   l = div(n,13)
 
-  nequ = 5*l + (l-1)
-  x0   = ones(n)
-  lvar = -Inf*ones(n)
-  uvar = +Inf*ones(n)
+  nls = Model()
 
-  F(x) = [sqrt(2)*[(3*x[i] - 60 + 1/10*(x[i+1] - x[i+2])^2) for i = 1:l];
-          sqrt(2)*[(x[i+1]^2 + x[i+2]^2 + (x[i+3]^2)*(1+x[i+3])^2 
-          + x[i+6] + x[i+5]/(1 + x[i+4]^2 + sin(x[i+4]/1000))) for i = 1:l];
-          sqrt(2)*[(x[i+6] + x[i+7] - x[i+8]^2 + x[i+10]) for i = 1:l];
-          sqrt(2)*[(log(1 + x[i+10]^2) + x[i+11] - 5*x[i+12] + 20) for i = 1:l];
-          sqrt(2)*[(x[i+4] + x[i+5] + x[i+5]*x[i+9] + 10*x[i+9] - 50) for i = 1:l]
-          if l > 1
-          	;sqrt(2)*[sum((x[i+6]-x[i+19]) for i = 1:l-1)]
-          end
-        ]
+  @variable(nls, x[i=1:n], start=1.0)
 
-  return ADNLSModel(F, x0, nequ, lvar=lvar, uvar=uvar, name="NZF1")
+  @NLexpression(nls, F1[i=1:l], sqrt(2)*(3*x[i] - 60 + 1/10*(x[i+1] - x[i+2])^2))
+  @NLexpression(nls, F2[i=1:l], sqrt(2)*(x[i+1]^2 + x[i+2]^2 + (x[i+3]^2)*(1+x[i+3])^2 
+                                + x[i+6] + x[i+5]/(1 + x[i+4]^2 + sin(x[i+4]/1000))))
+  @NLexpression(nls, F3[i=1:l], sqrt(2)*(x[i+6] + x[i+7] - x[i+8]^2 + x[i+10]))
+  @NLexpression(nls, F4[i=1:l], sqrt(2)*(log(1 + x[i+10]^2) + x[i+11] - 5*x[i+12] + 20))
+  @NLexpression(nls, F5[i=1:l], sqrt(2)*(x[i+4] + x[i+5] + x[i+5]*x[i+9] + 10*x[i+9] - 50))
+  if l > 1
+    @NLexpression(nls, F6, sqrt(2)*sum((x[i+6]-x[i+19]) for i = 1:l-1))
+    return MathProgNLSModel(nls, [F1; F2; F3; F4; F5; F6], name="NZF1")
+  else
+    return MathProgNLSModel(nls, [F1; F2; F3; F4; F5], name="NZF1")
+  end
 end

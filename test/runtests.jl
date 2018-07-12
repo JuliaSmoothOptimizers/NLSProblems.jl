e9f01aa4cd249df38f42e90f3370f9dc1c482a46
Xx-Alexis-xX <alexis.montoison@etu.enseeiht.fr>
2018-07-12 13:23:17 -0400

    Test that every problem can be instantiated with arguments

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,4 +19,7 @@ for prob in names(NLSProblems)
   ncx = m > 0 ? @sprintf("%10.4e", norm(cons(nls, x))) : "NA"
   @printf("%-15s  %4d  %4d  %4d  %10.4e  %10.4e  %10s\n",
           prob, N, n, m, nFx, JtF, ncx)
+  
+  # Test that every problem can be instantiated with arguments
+  nls2 = prob_fn(nls.meta.nvar)
 end

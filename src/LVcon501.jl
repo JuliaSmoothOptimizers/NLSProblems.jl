6f8c4e8089fbc9eb7d0491299678a59c07de9838
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-01-27 21:43:24 -0500

    Small fix on LVcon501

--- src/LVcon501.jl
+++ src/LVcon501.jl
@@ -25,7 +25,7 @@ function LVcon501(n :: Int=20)
   @variable(model, x[i=1:n], start=(i % 2 == 1 ? -1.2 : 1.0))
   @NLexpression(model, F1[i=1:n-1], 10 * (x[i]^2 - x[i+1]))
   @NLexpression(model, F2[i=1:n-1], x[i] - 1.0)
-  @NLconstraint(model, c[k=1:n-2], 3 * x[k+1]^3 - 2 * x[k+2] - 5 +
+  @NLconstraint(model, c[k=1:n-2], 3 * x[k+1]^3 + 2 * x[k+2] - 5 +
                        sin(x[k+1] - x[k+2]) * sin(x[k+1] + x[k+2]) +
                        4 * x[k+1] - x[k] * exp(x[k] - x[k+1]) - 3 == 0)
 

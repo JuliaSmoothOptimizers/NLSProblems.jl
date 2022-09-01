aca224f143056df45cb7f37c0c0d126b387417d4
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-09-01 16:03:33 -0400

    Update references in the README and the documentation

--- docs/make.jl
+++ docs/make.jl
@@ -4,14 +4,15 @@ using Documenter, NLSProblems
 makedocs(
   modules = [NLSProblems],
   doctest = true,
-  # linkcheck = true,
+  linkcheck = true,
   strict = true,
   format = Documenter.HTML(
     assets = ["assets/style.css"],
+    ansicolor = true,
     prettyurls = get(ENV, "CI", nothing) == "true",
   ),
   sitename = "NLSProblems.jl",
-  pages = Any[
+  pages = [
     "Home" => "index.md",
     "Tutorial" => "tutorial.md",
     "Benchmark" => "benchmark.md",
